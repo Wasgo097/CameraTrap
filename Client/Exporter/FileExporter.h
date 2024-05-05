af7e03a7d4c12f1a720e385354a1c85d0d23a090
Wasgo097 <patryk.pakula321@gmail.com>
2024-05-05 21:40:13 +0200

    FileExporter implementation

--- Client/Exporter/FileExporter.h
+++ Client/Exporter/FileExporter.h
@@ -10,6 +10,7 @@ public:
 	FileExporter(FileExporterSettings settings, std::unique_ptr<IDetectionResultSerializer>&& pSerializer);
 	void ExportData(const MoveDetectionResult& dataToExport) override;
 protected:
+	static std::string GetLocalTime();
 	FileExporterSettings _settings;
 	std::string _serializationBuffer;
 	std::unique_ptr<IDetectionResultSerializer> _pSerializer;
